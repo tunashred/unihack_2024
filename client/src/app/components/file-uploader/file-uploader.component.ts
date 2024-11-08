9bfd801377c76d56cf0c6c0723d881b8fb138885
Viorel <soituviorel@gmail.com>
2024-11-08 21:31:37 +0200

    project structure and file uploader component

---
+++ client/src/app/components/file-uploader/file-uploader.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'file-uploader',
+  standalone: true,
+  imports: [],
+  templateUrl: './file-uploader.component.html',
+  styles: ``
+})
+export class FileUploaderComponent {
+
+}
--- client/src/app/pages/home-page/home-page.component.ts
+++ client/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FileUploaderComponent } from "../../components/file-uploader/file-uploader.component";
 
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   templateUrl: './home-page.component.html',
-  styles: ``
+  styles: ``,
+  imports: [FileUploaderComponent]
 })
 export class HomePageComponent {
 
